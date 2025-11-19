905f356f32ecfa777701aeaf62ffa14ab2408719
ahsumon77 <ahazizulhakimsumon@gmail.com>
2025-11-19 22:01:53 +0600

    initial commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,12 @@
+import { createContext } from 'react'
+
+export const AppContext = createContext()
+
+export const AppContextProvider = () => {
+    const value = {
+
+    }
+    return (<AppContext.Provider value={value}>
+        {Props.children}
+    </AppContext.Provider>)
+}
