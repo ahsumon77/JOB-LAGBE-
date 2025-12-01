1b26fcd41bb465c37ce5627012a76014f675ea16
Abdullah Al Mamun <137890438+mamun2021331086@users.noreply.github.com>
2025-12-02 00:44:13 +0600

    userRoutes.js
    routes for user

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,24 @@
+import express from 'express'
+import { applyForJob, getUserData, getUserJobApplications, updateUserResume } from '../controllers/userController.js'
+import upload from '../config/multer.js'
+
+
+
+
+const router =express.Router()
+
+
+//Get user Data
+router.get('/user',getUserData)
+
+
+//Apply for a job
+router.post('/apply',applyForJob)
+
+// Get applied job data
+router.get('/applications',getUserJobApplications)
+
+//Update User profile (resume)
+router.post('/update-resume',upload.single('resume'),updateUserResume)
+
+export default router
